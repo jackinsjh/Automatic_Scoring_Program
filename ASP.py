e3941129c4fbb6f0d8d61cc5bb144a96f02be50c
jackinsjh <43871679+jackinsjh@users.noreply.github.com>
2019-06-17 18:37:15 +0900

    Import original codes

---
+++ ASP.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2
+
+from skimage.measure import compare_ssim
+import argparse
+import imutils
+
+# read unmarked image
+src = cv2.imread("unmarked.jpg", cv2.IMREAD_COLOR)
+height, width, channel = src.shape
+
+# assign 4 test paper's edges' coordinates and warp it to the original image size
+srcPoint=np.array([[66, 36], [699, 31], [734, 977], [41, 973]], dtype=np.float32)
+dstPoint=np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
+matrix = cv2.getPerspectiveTransform(srcPoint, dstPoint)
+# dstUnmarked : warped testing paper with no mark as original size
+dstUnmarked = cv2.warpPerspective(src, matrix, (width, height))
+
+
+# read marked image
+src = cv2.imread("marked.jpg", cv2.IMREAD_COLOR)
+height, width, channel = src.shape
+
+# assign 4 test paper's edges' coordinates and warp it to the original image size
+srcPoint=np.array([[210, 220], [641, 228], [682, 953], [50, 875]], dtype=np.float32)
+dstPoint=np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
+matrix = cv2.getPerspectiveTransform(srcPoint, dstPoint)
+# dstMarked : warped testing paper with markings as original size
+dstMarked = cv2.warpPerspective(src, matrix, (width, height))
+
+
+# convert the images to grayscale
+grayA = cv2.cvtColor(dstUnmarked, cv2.COLOR_BGR2GRAY)
+grayB = cv2.cvtColor(dstMarked, cv2.COLOR_BGR2GRAY)
+
+
+"""
+# blur
+grayA = cv2.GaussianBlur(grayA, (7, 7), 0)
+grayB = cv2.GaussianBlur(grayB, (7, 7), 0)
+"""
+
+
+# compute the Structural Similarity Index (SSIM) between the two
+# images, ensuring that the difference image is returned
+(score, diff) = compare_ssim(grayA, grayB, full=True)
+diff = (diff * 255).astype("uint8")
+print("SSIM: {}".format(score))
+
+
+
+# threshold the difference image, followed by finding contours to
+# obtain the regions of the two input images that differ
+thresh = cv2.threshold(diff, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+"""
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+
+# loop over the contours
+for c in cnts:
+    # compute the bounding box of the contour and then draw the
+    # bounding box on both input images to represent where the two
+    # images differ
+    (x, y, w, h) = cv2.boundingRect(c)
+    cv2.rectangle dstUnmarked, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.rectangle(dstMarked, (x, y), (x + w, y + h), (0, 0, 255), 2)
+"""
+
+# show the output images
+cv2.imshow("UnmarkedOriginal", grayA)
+cv2.imshow("MarkedOriginal", grayB)
+cv2.imshow("Diff", diff)
+cv2.imshow("Thresh", thresh)
+cv2.waitKey(0)
